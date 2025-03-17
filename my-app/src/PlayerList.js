130ce351dab6e006937945e440eb0e978c1a54ca
Maurice Mwenda <maur1cemmwenda@gmail.com>
2025-03-17 13:56:36 +0100

    Updates

--- my-app/src/PlayerList.js
+++ my-app/src/PlayerList.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Player from "./Player";
-import players from "./players"; // Import players array
+import footballers from "./footballers"; // Import footballers array
 
 const PlayersList = () => {
   return (
     <div className="container-fluid mt-4">
         <h1 className="text-center text-white fs-1 fst-italic text-decoration-underline">My Top 6</h1>
   <div className="row justify-content-center gap-2">
-    {players.map((player) => (
-      <div key={player.name} className="col-md-4 col-sm-6 col-12 d-flex justify-content-center">
-        <Player {...player} /> 
+    {footballers.map((footballer) => (
+      <div key={footballer.name} className="col-md-4 col-sm-6 col-12 d-flex justify-content-center">
+        <Player {...footballer} /> 
       </div>
     ))}
   </div>
